cd5e3e1c2b4b2fde82c23d9f48e31fb6a8dd6664
Nzubechi Ezekiel <105366169+kingsley214@users.noreply.github.com>
2022-06-25 02:27:39 +0100

    Add files via upload

---
+++ two.js
@@ -0,0 +1,13 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+let firstName = "NZUBECHI",
+    lastName = "EZEKIEL",
+    height = "7ft",
+    country = "Nigeria",
+
+    fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+console.log(height);
+console.log(country);
+
+alert(`${fullName} \n${height} \n${country}`)
